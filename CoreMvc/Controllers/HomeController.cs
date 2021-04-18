480bbd4f05f49f505b2cf452ca01883dcd7e2b47
Resul Kurtulmuşlu <resulkurtulmuslu@outlook.com>
2021-04-18 20:14:42 +0300

    volume, env

--- CoreMvc/Controllers/HomeController.cs
+++ CoreMvc/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using CoreMvc.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,15 +15,57 @@ namespace CoreMvc.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IFileProvider _fileProvider;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IFileProvider fileProvider)
         {
             _logger = logger;
+            _fileProvider = fileProvider;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult ImageSave()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ImageSave(IFormFile imageFile)
+        {
+            if (imageFile != null && imageFile.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(stream);
+                }
+            }
+
+            return View();
+        }
+
+        public IActionResult ShowImages()
+        {
+            var images = _fileProvider.GetDirectoryContents("wwwroot/images").ToList();
+
+            return View(images);
+        }
+
+        public IActionResult ImageDelete(string fileName)
+        {
+            var file = _fileProvider.GetDirectoryContents("wwwroot/images")
+                .ToList().First(x => x.Name == fileName);
+
+            System.IO.File.Delete(file.PhysicalPath);
+
+            return RedirectToAction("ShowImages");
         }
 
         public IActionResult Privacy()
